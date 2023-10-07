6a1ffd543c8cb16afcfb7b8649bfc34d469843de
余彬 <48550038+yubinyb@users.noreply.github.com>
2023-10-07 16:57:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,13 +30,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '8pWTE0TkidK6vl6d0eU9c4MplobetH3TIstyX7jIx1M',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oeOCc6HpzAOgvE3i2Telb9BpCt9o',
     }
   ],
 
